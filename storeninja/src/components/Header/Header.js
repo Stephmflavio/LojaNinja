04f5355a2b9e4df604f9db9195b5e8b262874327
Stephamflavio <Stephdpmello@gmail.com>
2022-10-18 14:33:20 -0300

    Criação de rotas entre home e carrinho

---
+++ storeninja/src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { HeaderContainer } from './styled'
+
+const Header = (props) => {
+    return(
+        <HeaderContainer>
+            <h2>Ninja</h2>
+            <div>
+                <button onClick={() => props.changePage("home")}>Home</button>
+                <button onClick={() => props.changePage("cart")}>Carrinho</button>
+            </div>
+        </HeaderContainer>
+            
+    )
+}
+
+export default Header
